fac3c51042db6727abad6e8bd9e7fdb1fb73d5c6
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-09 11:33:38 +0900

    動かなくなっていたので修正

--- src/pages/post/index.vue
+++ src/pages/post/index.vue
@@ -4,15 +4,9 @@
 
 <script>
 import Header from '../../components/Header.vue';
-import Footer from '../../components/Footer.vue';
-import SideMenu from '../../components/SideMenu.vue';
-import Detail from '../../components/Detail.vue';
 
 export default {
   components: {
-    Header,
-    Footer,
-    SideMenu,
     Detail
   },
   data () {
